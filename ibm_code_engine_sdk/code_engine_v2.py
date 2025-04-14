fa1ca34528dee22f6d5e1cb3c7fa56d281d30f0e
IBM Cloud Code Engine UX Automation <ce.ui.team@de.ibm.com>
2025-04-14 20:59:45 +0000

    feat(api): Updated python-sdk to adopt Code Engine API specification changes

--- ibm_code_engine_sdk/code_engine_v2.py
+++ ibm_code_engine_sdk/code_engine_v2.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# IBM OpenAPI SDK Code Generator Version: 3.99.0-d27cee72-20250129-204831
+# IBM OpenAPI SDK Code Generator Version: 3.102.0-615ec964-20250307-203034
 
 """
 REST API for Code Engine
